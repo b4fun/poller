5594dd72a45cc9d419054f6b200f6561e6397746
hbc <bcxxxxxx@gmail.com>
2022-02-05 23:19:32 +0800

    feat: define entity types

---
+++ poller/entity.py
@@ -0,0 +1,33 @@
+import abc
+import dataclasses
+
+
+class Entity(metaclass=abc.ABCMeta):
+    """Entity describes the entity for polling."""
+
+    @property
+    @abc.abstractmethod
+    def id(self) -> str:
+        """Unique ID of the entity."""
+
+    @abc.abstractmethod
+    def to_json(self) -> str:
+        """Convert the entity instance to JSON."""
+
+
+@dataclasses.dataclass
+class EntityStorageSettings:
+    """Base settings for the entity storage."""
+
+    # table to store the entity.
+    table: str
+
+
+class EntityStorage(metaclass=abs.ABCMeta):
+    """EntityStorage defines the base interface for the entity storage."""
+    
+    def save_entity(self, entity: Entity):
+        """Save an entity."""
+
+    def has_entity(self, entity: Entity) -> bool:
+        """Check if an entity is already stored."""
